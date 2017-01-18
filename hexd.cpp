9b0356be08107ee57eef42a9425989598aac7227
TechnoSam <technosam4jc@gmail.com>
2017-01-17 21:18:27 -0500

    Setup project
    Created Visual Studio solution file with hexd.cpp in the repo root
    directory. Added appropriate VS .gitignore file.

---
+++ hexd.cpp
@@ -0,0 +1,16 @@
+// File:		hexd.cpp
+// Author:		Samuel McFalls
+// Date:		01/27/17
+// Description:	Project 0: Hexdump
+//				Prints out a hex dump of a given input file.
+//				Usage: hexdump.exe <filename>
+
+#include <iostream>
+#include <cstdlib>
+
+int main(int arc, char* argv[]) {
+
+
+
+	return EXIT_SUCCESS;
+}
