3f711f0f4376bc499aa1cf6d364664b92f65e9d1
TechnoSam <technosam4jc@gmail.com>
2017-01-17 23:35:51 -0500

    Enable binary reading and hex printing
    Reads the file in binary and prints the hex value for each character if
    it's within the printable range. Else prints '.'. Also updated error
    message for incorrect argc.

--- hexd.cpp
+++ hexd.cpp
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include <string>
+#include <cstdint>
 #include <fstream>
 #include <cstdlib>
 
@@ -14,30 +15,34 @@ int main(int argc, char* argv[]) {
 
 	// If an incorrect number of arguments is given, we can immediately terminate.
 	if (argc != 2) {
+		std::cout << "Usage: hexdump <filename>\n";
 		return EXIT_FAILURE;
 	}
 
 	const std::string FILENAME = argv[1];
 
-	std::ifstream inFile;
+	std::ifstream inStream;
 
-	inFile.open(FILENAME);
+	inStream.open(FILENAME, std::ios::binary);
 
-	if (!inFile.is_open()) {
+	if (!inStream.is_open()) {
 		std::cout << "Could not open \"" << FILENAME << "\". It does not exist or contains errors.";
 		return EXIT_FAILURE;
 	}
 
-	std::string buffer;
+	// I'm not sure why we need 3 elements in the buffer
+	char buffer[3];
+	uint8_t value;
 
-	while (!inFile.eof()) {
+	while (inStream.read(reinterpret_cast<std::fstream::char_type*>(&value), sizeof value)) {
 
-		std::getline(inFile, buffer, '\n');
-
-		// If the line contains nothing (not even a newline) then we must be at the end of the file
-		if (buffer == "") { break; }
-
-		std::cout << buffer << "\n";
+		if (value > 31 && value < 127) {
+			std::sprintf(buffer, "%02X", value);
+			std::cout << buffer << "\n";
+		}
+		else {
+			std::cout << ".\n";
+		}
 
 	}
 
