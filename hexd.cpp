2bda867bccf9197e81bee9a9c0755afa0c8a0eed
TechnoSam <technosam4jc@gmail.com>
2017-01-17 22:39:56 -0500

    Enable file reading
    Hexd now takes an input file and prints it back to stdout. If there is
    an error in opening the file, an appropriate error is returned. Also
    updated the header comments.

--- hexd.cpp
+++ hexd.cpp
@@ -3,14 +3,43 @@
 // Date:		01/27/17
 // Description:	Project 0: Hexdump
 //				Prints out a hex dump of a given input file.
-//				Usage: hexdump.exe <filename>
+//				Usage: hexdump <filename>
 
 #include <iostream>
+#include <string>
+#include <fstream>
 #include <cstdlib>
 
-int main(int arc, char* argv[]) {
+int main(int argc, char* argv[]) {
 
+	// If an incorrect number of arguments is given, we can immediately terminate.
+	if (argc != 2) {
+		return EXIT_FAILURE;
+	}
 
+	const std::string FILENAME = argv[1];
+
+	std::ifstream inFile;
+
+	inFile.open(FILENAME);
+
+	if (!inFile.is_open()) {
+		std::cout << "Could not open \"" << FILENAME << "\". It does not exist or contains errors.";
+		return EXIT_FAILURE;
+	}
+
+	std::string buffer;
+
+	while (!inFile.eof()) {
+
+		std::getline(inFile, buffer, '\n');
+
+		// If the line contains nothing (not even a newline) then we must be at the end of the file
+		if (buffer == "") { break; }
+
+		std::cout << buffer << "\n";
+
+	}
 
 	return EXIT_SUCCESS;
 }
